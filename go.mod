268e08ffc007f9e663b6d6623ccef9395cd19d78
Paul Biggar <paul.biggar@gmail.com>
2021-09-04 19:10:17 -0400

    write to file

--- go.mod
+++ go.mod
@@ -5,11 +5,20 @@ go 1.17
 require gopkg.in/src-d/go-git.v4 v4.13.1
 
 require (
+	github.com/cosmtrek/air v1.27.3 // indirect
+	github.com/creack/pty v1.1.15 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-git/go-billy v4.2.0+incompatible // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/src-d/gcfg v1.4.0 // indirect
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	gopkg.in/src-d/go-billy.v4 v4.3.2 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- toc.go
+++ toc.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/go-git/go-billy/osfs"
 	"gopkg.in/src-d/go-git.v4/plumbing/format/gitignore"
@@ -23,6 +26,8 @@ type Record struct {
 	description string
 }
 
+type Records = map[string]Record
+
 func initializeGitIgnores(dir string) {
 	fs := osfs.New(dir)
 	patterns, err := gitignore.ReadPatterns(fs, []string{"."})
@@ -31,23 +36,35 @@ func initializeGitIgnores(dir string) {
 	check(err)
 }
 
-var records map[string]Record
+func printRecords(records Records) {
+	f, err := os.Create("TOC.md")
+	check(err)
+	w := bufio.NewWriter(f)
 
-func writeRecord(path string, description string) {
-	records[path] = Record{path: path, description: description}
+	for _, r := range records {
+		w.WriteString(r.path)
+		w.WriteString(": ")
+		w.WriteString(r.description)
+		w.WriteString("\n")
+	}
+	w.Flush()
 }
 
 func main() {
 	flag.Parse()
 	dir := flag.Arg(0)
-	records = make(map[string]Record)
+	records := make(map[string]Record)
 
 	initializeGitIgnores(dir)
 
+	fmt.Printf("reading %q\n", dir)
+
 	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		check(err)
+		path = strings.TrimPrefix(path, dir)
+		path = strings.TrimPrefix(path, "/")
 		// Check if it's ignored
-		if ignoreMatcher.Match([]string{path}, d.IsDir()) {
+		if ignoreMatcher.Match([]string{path}, d.IsDir()) || path == ".git" {
 			fmt.Printf("skipping: %q\n", path)
 			if d.IsDir() {
 				return filepath.SkipDir
@@ -56,11 +73,11 @@ func main() {
 			}
 		}
 		if d.IsDir() {
-			desc := "a dir named" + path
-			writeRecord(path, desc)
+			desc := "a dir named " + path
+			records[path] = Record{path: path, description: desc}
 		} else {
-			desc := "a file named" + path
-			writeRecord(path, desc)
+			desc := "a file named " + path
+			records[path] = Record{path: path, description: desc}
 		}
 		return nil
 	})
@@ -68,6 +85,6 @@ func main() {
 		fmt.Printf("error walking the path %q: %v\n", '.', err)
 		return
 	} else {
-		fmt.Printf("done %v\n", records)
+		printRecords(records)
 	}
 }
